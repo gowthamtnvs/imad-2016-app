77613ac6ccacae0e23b5e431bd3ecf9814cc7deb
gowthamtnvs <gowthamtnvs98@gmail.com>
2016-09-25 11:14:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile('hello friend you are on the right website for article one');
+});
+app.get('/article-three',function(req, res){
+     res.sendFile('you are on the article 3 page');
+});
+app.get('/article-two', function (req, res) {
+  res.sendFile('hello friend you are on the right website for article two');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
